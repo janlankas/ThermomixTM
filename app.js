04c1ece1a0dbac068f48ba95c9471603195c9a96
janlankas <lankascb@gmail.com>
2022-03-18 20:40:15 +0100

    galerie

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ document.querySelectorAll("video").forEach(vid => {
 
 document.querySelector('.popup-video span').onclick = () => {
     document.querySelector(".popup-video").style.display = "none";
+    document.querySelector(".popup-video video").pause();
 }
 
 
+
+//Image popup
+
+document.querySelectorAll(".video img").forEach(img => {
+    img.onclick = () => {
+        document.querySelector(".popup-img").style.display = "block";
+        document.querySelector(".popup-img img").src = img.getAttribute("src");
+    }
+})
+
+document.querySelector('.popup-img span').onclick = () => {
+    document.querySelector(".popup-img").style.display = "none";
+}
 
