b52877e8075f79a93fb257e5a3c5b7438aa8a9e1
janlankas <lankascb@gmail.com>
2022-04-02 21:59:47 +0200

    again

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ document.querySelectorAll("video").forEach(vid => {
     vid.onclick = () => {
         document.querySelector(".popup-video").style.display = "block";
         document.querySelector(".popup-video video").src = vid.getAttribute("src");
+        document.querySelector(".popup-video video").style.position = "absolute";
+
     }
 });
 
