ded44f49b2ed6a7ee33b3b298ce3cb5260250809
janlankas <lankascb@gmail.com>
2022-04-02 22:41:31 +0200

    jotakhle

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 
-document.querySelectorAll("video").forEach(vid => {
-    vid.onclick = () => {
-        document.querySelector(".popup-video").style.display = "block";
-        document.querySelector(".popup-video video").src = vid.getAttribute("src");
-        document.querySelector(".popup-video video").style.position = "absolute";
+// document.querySelectorAll("video").forEach(vid => {
+//     vid.onclick = () => {
+//         document.querySelector(".popup-video").style.display = "block";
+//         document.querySelector(".popup-video video").src = vid.getAttribute("src");
+//         document.querySelector(".popup-video video").style.position = "absolute";
 
-    }
-});
+//     }
+// });
 
-document.querySelector('.popup-video span').onclick = () => {
-    document.querySelector(".popup-video").style.display = "none";
-    document.querySelector(".popup-video video").pause();
-}
+// document.querySelector('.popup-video span').onclick = () => {
+//     document.querySelector(".popup-video").style.display = "none";
+//     document.querySelector(".popup-video video").pause();
+// }
 
+
+// document.querySelectorAll("video").forEach(vid => {
+//     vid.onmouseover = () => {
+//         document.querySelectorAll(".video video").setAttribute("controls", "true");
+
+//     }
+// });
 
 
 //Image popup
